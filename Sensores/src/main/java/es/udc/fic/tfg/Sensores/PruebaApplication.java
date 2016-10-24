765d3936aaa7c9edfcfb68948678508bc938c895
Heidy Mabel Izaguirre <hmiacatracha@gmail.com>
2016-10-24 17:07:55 +0200

    Proyecto inicial!

---
+++ Sensores/src/main/java/es/udc/fic/tfg/Sensores/PruebaApplication.java
@@ -0,0 +1,12 @@
+package es.udc.fic.tfg.prueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaApplication.class, args);
+	}
+}
---
+++ Sensores/src/main/java/es/udc/fic/tfg/Sensores/ServletInitializer.java
@@ -0,0 +1,13 @@
+package es.udc.fic.tfg.prueba;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PruebaApplication.class);
+	}
+
+}
