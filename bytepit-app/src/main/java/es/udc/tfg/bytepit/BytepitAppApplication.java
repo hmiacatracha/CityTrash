c03a8eda287ff18e8987605d86437cf5e1977376
Heidy Mabel Izaguirre <hmiacatracha@gmail.com>
2017-02-05 14:01:34 +0100

    Generando el proyecto inicial

---
+++ bytepit-app/src/main/java/es/udc/tfg/bytepit/BytepitAppApplication.java
@@ -0,0 +1,12 @@
+package es.udc.tfg.bytepit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BytepitAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BytepitAppApplication.class, args);
+	}
+}
