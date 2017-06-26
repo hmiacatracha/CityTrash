682b95250f3eee745574d7638f62ada5f9d1c28b
Heidy Mabel Izaguirre <hmiacatracha@gmail.com>
2017-06-26 18:07:41 +0200

    Borrando proyecto

--- CONSUMER/tfg-app/src/main/es/udc/tfg/service/ConsumerService.java
+++
@@ -1,9 +0,0 @@
-package es.udc.tfg.service;
-
-import es.udc.tfg.util.exceptions.DuplicateInstanceException;
-import es.udc.tfg.model.type.*;
-
-public interface ConsumerService {
-
-	public Type registerType(String type, String description) throws DuplicateInstanceException;
-}
--- CONSUMER/tfg-app/src/main/es/udc/tfg/util/exceptions/InstanceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package es.udc.tfg.util.exceptions;
-
-@SuppressWarnings("serial")
-public class InstanceNotFoundException extends InstanceException {
-
-	public InstanceNotFoundException(Object key, String className) {
-		super("Instance not found", key, className);
-	}
-}
